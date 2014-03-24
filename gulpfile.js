0b81fbf8a25098f42262950b0cd5bd9bdca9bb81
Carsten Lamm <CarstenLamm@googlemail.com>
2014-03-24 20:20:41 +0100

    some changes
    - adds .jshintrc
    - use gulp-ruby-haml instead of gulp-haml

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,70 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var haml = require('gulp-haml');
+var haml = require('gulp-ruby-haml');
 var minifyCSS = require('gulp-minify-css');
 var gulpBowerFiles = require('gulp-bower-files');
+var express = require("express");
+var concat = require('gulp-concat');
+var help = require('gulp-task-listing');
+var clean = require('gulp-clean');
 
+// ASSETS
+gulp.task('build', ['build_html', 'build_css', 'build_bowerfiles', 'build_javascript', 'build_images']);
 
-gulp.task('haml', function() {
-  gulp.src('app/templates/*.haml')
-    .pipe(haml())
-    .pipe(gulp.dest('build/templates'));
-
-  gulp.src('app/*.haml')
+gulp.task('build_html', function () {
+  gulp.src('app/**/*.haml')
     .pipe(haml())
     .pipe(gulp.dest('build'));
 });
 
-
-gulp.task('scss', function() {
+gulp.task('build_css', function () {
   gulp.src('app/styles/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/styles'));
 });
 
-
-gulp.task("bower-files", function() {
-  gulp.src('app/styles/*.scss')
-    .pipe(sass())
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('build/styles'));
-});
-
-gulp.task("script", function() {
+gulp.task('build_bowerfiles', function () {
   gulpBowerFiles().pipe(gulp.dest("build/lib"));
 });
 
-gulp.task("images", function() {
+gulp.task('build_javascript', function () {
+  gulp.src('scripts/*.js')
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task("build_images", function () {
   gulp.src('app/images/*')
     .pipe(gulp.dest('build/images'))
 });
 
 
-gulp.task('build', function() {
-  gulp.run('haml', 'scss', 'bower-files', 'images')
+// CLEAN
+gulp.task('clean', function () {
+  gulp.src('build', {read: false})
+    .pipe(clean());
 });
 
 
-gulp.task('default', function() {
-  gulp.run('build')
+// SERVER
+gulp.task('server', function() {
+  var app = express();
+  app.use(express.static(__dirname + "/build/"));
+  console.log('server runs on port 9000')
+  app.listen(9000);
 });
+
+gulp.task('watch', ['server'], function () {
+  gulp.watch('app/**/*.haml', ['build_html']);
+  gulp.watch('app/styles/**/*.scss', ['build_css']);
+  gulp.watch('app/scripts/**/*.js', ['build_javascript']);
+  gulp.watch('app/images/**/*', ['build_images']);
+  gulp.watch('app/bower_components/*', ['build_bowerfiles']);
+});
+
+
+gulp.task('default', help)
+gulp.task('help', help)
